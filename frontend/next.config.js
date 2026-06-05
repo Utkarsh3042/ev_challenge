ed24bc37b0baa89ed078e134eedb994e837ce847
Utkarsh Saraswat <u.saraswat30@gmail.com>
2026-06-05 16:50:28 +0530

    frontend issue

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -9,18 +9,4 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
 };
 
-const { withSentryConfig } = require('@sentry/nextjs');
-
-module.exports = withSentryConfig(
-  withNextIntl(nextConfig),
-  {
-    silent: true,
-    org: process.env.SENTRY_ORG,
-    project: process.env.SENTRY_PROJECT,
-    widenClientFileUpload: true,
-    transpileClientSDK: true,
-    hideSourceMaps: true,
-    disableLogger: true,
-    autoInstrumentMiddleware: false,
-  }
-);
+module.exports = withNextIntl(nextConfig);
