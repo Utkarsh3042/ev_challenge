88bdbb02d57a8dc029a37fdf77e869620a4fe211
Utkarsh Saraswat <u.saraswat30@gmail.com>
2026-06-05 17:04:11 +0530

    fix issue

--- frontend/lib/i18n-config.ts
+++ frontend/lib/i18n-config.ts
@@ -6,8 +6,22 @@ export default getRequestConfig(async ({ requestLocale }) => {
   if (!locale || !routing.locales.includes(locale as any)) {
     locale = routing.defaultLocale
   }
+  let messages;
+  switch (locale) {
+    case 'hi':
+      messages = (await import('../messages/hi.json')).default;
+      break;
+    case 'kn':
+      messages = (await import('../messages/kn.json')).default;
+      break;
+    case 'en':
+    default:
+      messages = (await import('../messages/en.json')).default;
+      break;
+  }
+
   return {
     locale,
-    messages: (await import(`../messages/${locale}.json`)).default,
+    messages,
   }
 })
